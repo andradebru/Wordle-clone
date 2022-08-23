6da38ce513acf13ee2361266cf10ff79dddd759f
andradebru <brunansandrade@gmail.com>
2022-08-23 15:44:59 -0300

    insere link para o repo e tenta colorir as letras do acerto

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ const palavrasValidas = [
   "tempo",
 ];
 
-const palavraDoDia = "FESTA";
+const palavraDoDia = "festa";
 let linha = 1;
 let entrada = [];
 let tentativas = 6;
@@ -114,7 +114,6 @@ const ouvinteDeTeclas = (event) => {
     console.log(entrada);
   } else if (entrada.length == 5) {
     console.log("Só aceita até 5 letras");
-    validarEntrada();
   } else {
     entrada.push(char);
     console.log(entrada);
@@ -138,36 +137,52 @@ function apagaLetra() {
     console.log("não há nada para apagar");
   }
 }
+
+// function coloreTeclado() {
+//   let botaoTeclado = document.getElementsByClassName('letra');
+// }
+
 function validarEntrada() {
   console.log(
     "validar se " + entrada.join("") + " é igual " + palavraDoDia.toUpperCase()
   );
+  // se a entrada for extamaente a palavra do dia:
   if (entrada.join("") == palavraDoDia.toUpperCase()) {
-    // se a entrada for extamaente a palavra do dia
+    modal.style.display = "block"; // mostra o modal quando acerta
     for (let letra = 0; letra < entrada.length; letra++) {
+      let botaoTeclado = document.getElementById(entrada[letra]);
       let elId = `c${letra + 1}l${linha}`;
       const el = document.getElementById(elId);
       el.classList.add("validado", "rotate-horizontal-center");
-      modal.style.display = "block"; // mostra o modal quando acerta
       tentativas = 0;
-      //TODO uma maneira de bloquear as tentativas depois de acertar
+      botaoTeclado.classList.remove("normal");
+      botaoTeclado.classList.add("validados");
     }
   } else {
     for (let letra = 0; letra < entrada.length; letra++) {
       let elId = `c${letra + 1}l${linha}`;
       const el = document.getElementById(elId);
       let palavraDoDia1 = palavraDoDia.toUpperCase();
+      let botaoTeclado = document.getElementById(entrada[letra]);
+      // se a letra for igual:
       if (entrada[letra] == palavraDoDia[letra].toUpperCase()) {
         el.classList.add("validado", "rotate-horizontal-center");
+        botaoTeclado.classList.remove("normal");
+        botaoTeclado.classList.add("validado");
       } else if (palavraDoDia1.includes(entrada[letra])) {
-        // se a letra existe na palavra mas está no lugar errado
+        // se a letra existe na palavra mas está no lugar errado:
         let tentativaLetraPosicao = entrada[letra];
         let posicaoRealLetra = palavraDoDia.lastIndexOf(palavraDoDia[letra]);
         if (tentativaLetraPosicao != posicaoRealLetra) {
           el.classList.add("posicao-errada", "rotate-horizontal-center");
+          botaoTeclado.classList.remove("normal");
+          botaoTeclado.classList.add("posicao-errada");
         }
+        // se a letra for invalida:
       } else {
         el.classList.add("invalido", "rotate-horizontal-center");
+        botaoTeclado.classList.remove("normal");
+        botaoTeclado.classList.add("invalido");
       }
     }
     tentativas--;
---
+++ todo.md
@@ -0,0 +1,14 @@
+colorir letras do teclado
+teclado onclick
+
+<!-- mais palavras-->
+
+trocar palavras
+
+<!-- parar jogo dps do acerto -->
+
+localstorage = armazenar estado no navegador
+
+<!-- trocar alert para modal -->
+
+deixar o codigo non Case sensitve
